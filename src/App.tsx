5c9b21132bcd52d5c50e90c29ca2863851852087
piciliano <picilianovasconcelos@gmail.com>
2025-01-14 15:52:29 -0300

    refactor: lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "react-hot-toast";
+
 import AboutMe from "./components/aboutMe";
 import Beginning from "./components/beginning";
 import ContactMe from "./components/contactMe";
@@ -6,20 +7,26 @@ import { Line } from "./components/line/styled";
 import MyProjects from "./components/myProjects";
 import MySkill from "./components/mySkills";
 import Navbar from "./components/navbar";
+import React from "react";
 
 function App() {
+  const sections = [
+    { id: "about", Component: AboutMe },
+    { id: "projects", Component: MyProjects },
+    { id: "skills", Component: MySkill },
+    { id: "contact", Component: ContactMe },
+  ];
+
   return (
     <>
       <Navbar />
       <Beginning />
-      <Line id="about" />
-      <AboutMe />
-      <Line id="projects" />
-      <MyProjects />
-      <Line id="skills" />
-      <MySkill />
-      <Line id="contact" />
-      <ContactMe />
+      {sections.map(({ id, Component }) => (
+        <React.Fragment key={id}>
+          <Line id={id} />
+          <Component />
+        </React.Fragment>
+      ))}
       <Toaster position="top-right" reverseOrder={false} />
     </>
   );
