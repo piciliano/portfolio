e1c8a553d1acb1683ab1ade59387b27ee5134f04
piciliano <picilianovasconcelos@gmail.com>
2025-01-14 15:54:04 -0300

    refactor: clean code

--- src/components/beginning/index.tsx
+++ src/components/beginning/index.tsx
@@ -2,63 +2,54 @@ import * as S from "./styled";
 import homePageImg from "../../assets/homepageimg.svg";
 import { SocialIcon } from "react-social-icons";
 
+const SOCIAL_LINKS = [
+  { url: "https://github.com/piciliano", color: "#333" },
+  { url: "https://www.linkedin.com/in/picilianovasconcelos", color: "#0077b5" },
+  { url: "https://www.instagram.com/SeuPerfil", color: "#E4405F" },
+];
+
 const Beginning: React.FC = () => {
+  const handleScrollToContact = () => {
+    document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <>
-      <S.Section>
-        <S.BeginningContent>
-          <S.Title>Olá, Meu Nome é Neto</S.Title>
-          <S.SubTitle>Desenvolvedor Fullstack</S.SubTitle>
-          <S.Description>
-            Bem-vindo ao meu portfólio! Eu sou Neto, um desenvolvedor web
-            full-stack focado em criar aplicações web eficientes, escaláveis e
-            dinâmicas. Com habilidades em diversas tecnologias, incluindo
-            front-end como HTML, CSS e JavaScript, e back-end utilizando
-            Node.js, Express e bancos de dados, estou comprometido em oferecer
-            soluções inovadoras e responsivas. Neste portfólio, você encontrará
-            diversos projetos que abrangem o desenvolvimento de aplicações web
-            de ponta a ponta, desde o design de interfaces atraentes até a
-            implementação de lógica de negócios robusta. Explore meus trabalhos
-            e descubra como posso ajudar a transformar suas ideias digitais em
-            soluções com performance otimizada e design intuitivo.
-          </S.Description>
-          <S.ContainerButtons>
-            <S.Button
-              onClick={() =>
-                document
-                  .getElementById("contact")
-                  ?.scrollIntoView({ behavior: "smooth" })
-              }
-            >
-              Contato
-            </S.Button>
-            <S.LinkButtons>
+    <S.Section>
+      <S.BeginningContent>
+        <S.Title>Olá, Meu Nome é Neto</S.Title>
+        <S.SubTitle>Desenvolvedor Fullstack</S.SubTitle>
+        <S.Description>
+          Bem-vindo ao meu portfólio! Eu sou Neto, um desenvolvedor web
+          full-stack focado em criar aplicações web eficientes, escaláveis e
+          dinâmicas. Com habilidades em diversas tecnologias, incluindo
+          front-end como HTML, CSS e JavaScript, e back-end utilizando Node.js,
+          Express e bancos de dados, estou comprometido em oferecer soluções
+          inovadoras e responsivas. Neste portfólio, você encontrará diversos
+          projetos que abrangem o desenvolvimento de aplicações web de ponta a
+          ponta, desde o design de interfaces atraentes até a implementação de
+          lógica de negócios robusta. Explore meus trabalhos e descubra como
+          posso ajudar a transformar suas ideias digitais em soluções com
+          performance otimizada e design intuitivo.
+        </S.Description>
+        <S.ContainerButtons>
+          <S.Button onClick={handleScrollToContact}>Contato</S.Button>
+          <S.LinkButtons>
+            {SOCIAL_LINKS.map(({ url, color }, index) => (
               <SocialIcon
-                url="https://github.com/piciliano"
-                style={{ margin: "10px" }}
-                bgColor="#333"
+                key={index}
+                url={url}
+                bgColor={color}
                 fgColor="#fff"
-              />
-              <SocialIcon
-                url="https://www.linkedin.com/in/picilianovasconcelos"
                 style={{ margin: "10px" }}
-                bgColor="#0077b5"
-                fgColor="#fff"
               />
-              <SocialIcon
-                url="https://www.instagram.com/SeuPerfil"
-                style={{ margin: "10px" }}
-                bgColor="#E4405F"
-                fgColor="#fff"
-              />
-            </S.LinkButtons>
-          </S.ContainerButtons>
-        </S.BeginningContent>
-        <S.BeginningProfile>
-          <S.HomePageImg src={homePageImg} alt="pageImg" />
-        </S.BeginningProfile>
-      </S.Section>
-    </>
+            ))}
+          </S.LinkButtons>
+        </S.ContainerButtons>
+      </S.BeginningContent>
+      <S.BeginningProfile>
+        <S.HomePageImg src={homePageImg} alt="Imagem do Perfil" />
+      </S.BeginningProfile>
+    </S.Section>
   );
 };
 
