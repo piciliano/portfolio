63f1790170b93d59933559df4109c4f0daecd28f
piciliano <picilianovasconcelos@gmail.com>
2025-01-15 16:56:35 -0300

    feat: added footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Footer: React.FC = () => {
+  return (
+    <S.FooterContainer>
+      © 2025 Todos os direitos reservados
+    </S.FooterContainer>
+  );
+};
+
+export default Footer;
